91620f5110fa023b1bf25888809c073337509f22
Shareef Syed <shareefbasha53@gmail.com>
2022-02-26 01:10:53 +0530

    Convert v1

--- utils/send_ding_msg.py
+++ utils/send_ding_msg.py
@@ -6,16 +6,16 @@ from django.conf import settings
 os.environ['DJANGO_SETTINGS_MODULE'] = 'db_monitor.settings'
 
 def send_ding_msg(content):
-# 请求的URL，WebHook地址
+# The requested URL, WebHook address
     webhook = settings.DING_WEBHOOK
     is_send_ding_msg = settings.IS_SEND_DING_MSG
 
-#构建请求头部
+# build request header
     header = {
         "Content-Type": "application/json",
         "Charset": "UTF-8"
 }
-#构建请求数据
+#Build request data
     message ={
 
         "msgtype": "text",
@@ -28,16 +28,16 @@ def send_ding_msg(content):
         }
 
     }
-#对请求的数据进行json封装
+#Json request data encapsulation
     message_json = json.dumps(message)
     
     if is_send_ding_msg == 1:
-#发送请求
+#Send the request
         info = requests.post(url=webhook,data=message_json,headers=header) 
-#打印返回的结果
-        print('钉钉告警发送：{}'.format(info.text))
+#Print the results returned
+        print('Nailing the alarm sent：{}'.format(info.text))
 
 
 if __name__=="__main__":
-    content = '告警测试'
+    content = 'The alarm test'
     send_ding_msg(content)
